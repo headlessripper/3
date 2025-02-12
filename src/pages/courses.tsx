fbc57d620e52106a84f838b22f6f1bf6623f8718
headlessripper <167107185+headlessripper@users.noreply.github.com>
2025-02-12 20:33:13 +0200

    icon fixed

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -13,7 +13,7 @@ export default function ServicesPage() {
           content="OnePro Nextjs Starter Template is a free template designed for business landing pages, which aligns with the growing trend towards one-page web designs."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/bobaas.ico" />
       </Head>
       <Header />
       <PageHeader title="Our Courses" navTitle="Courses" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
         <title>Bobaas Academy of Excellence</title>
         <meta name="description" content="OnePro Nextjs Starter Template is a free template designed for business landing pages, which aligns with the growing trend towards one-page web designs." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/bobaas.ico" />
       </Head>
       <Header />
       <HomeBanner />
